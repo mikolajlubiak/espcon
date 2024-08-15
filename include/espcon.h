a17353abf59539823c53980a9a5d9a0f695ede3e
Mikołaj Lubiak <lubiak@proton.me>
2024-08-15 21:39:13 +0200

    Add movement

--- include/espcon.h
+++ include/espcon.h
@@ -8,9 +8,17 @@
 #include <Adafruit_ILI9341.h>
 #endif
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "camera.h"
+
 constexpr uint8_t tftCS = 10;
 constexpr uint8_t tftDC = 9;
 constexpr uint8_t tftRST = 8;
+
+constexpr uint8_t buttons[] = {41, 42};
 
 constexpr uint16_t fps = 60;
 constexpr float frame_delay = static_cast<float>(fps) / 1000;
@@ -35,19 +43,9 @@ struct color
     }
 };
 
-struct vec3d
-{
-    float x, y, z;
-};
-
-struct mat4
-{
-    float m[4][4] = {0.0f};
-};
-
 struct triangle
 {
-    vec3d p[3];
+    glm::vec4 p[3];
     uint16_t col;
 };
 
@@ -112,15 +110,15 @@ mesh *initMeshCube()
     mMesh->numTris = numTris;
 
     // Cube vertices
-    const vec3d vertices[8] = {
-        {0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 1.0f},
-        {0.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 0.0f},
-        {1.0f, 0.0f, 0.0f},
-        {0.0f, 1.0f, 0.0f},
-        {1.0f, 0.0f, 1.0f}};
+    const glm::vec4 vertices[8] = {
+        {0.0f, 0.0f, 0.0f, 1.0f},
+        {0.0f, 0.0f, 1.0f, 1.0f},
+        {0.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 1.0f, 0.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f, 1.0f},
+        {0.0f, 1.0f, 0.0f, 1.0f},
+        {1.0f, 0.0f, 1.0f, 1.0f}};
 
     // Triangle indices
     int triIndices[numTris][3] = {
@@ -185,7 +183,7 @@ mesh *loadObj(const char *path)
     uint32_t numFilledTris = 0;
 
     uint32_t numAllocVerts = numLoadVerts;
-    vec3d *verts = reinterpret_cast<vec3d *>(calloc(numAllocVerts, sizeof(vec3d)));
+    glm::vec3 *verts = reinterpret_cast<glm::vec3 *>(calloc(numAllocVerts, sizeof(glm::vec3)));
     if (verts == nullptr)
     {
         Serial.println("Error in loadObj, verts == nullptr");
@@ -220,7 +218,7 @@ mesh *loadObj(const char *path)
                     if (numFilledVerts > numAllocVerts)
                     {
                         numAllocVerts = numAllocVerts * 2;
-                        verts = reinterpret_cast<vec3d *>(realloc(verts, numAllocVerts * sizeof(vec3d)));
+                        verts = reinterpret_cast<glm::vec3 *>(realloc(verts, numAllocVerts * sizeof(glm::vec3)));
                         if (verts == nullptr)
                         {
                             Serial.println("Error in loadObj, verts == nullptr after realloc");
@@ -232,7 +230,7 @@ mesh *loadObj(const char *path)
                 {
                     int f[3];
                     sscanf(line + 2, "%d %d %d", &f[0], &f[1], &f[2]);
-                    mMesh->tris[numFilledTris] = {verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1]};
+                    mMesh->tris[numFilledTris] = {glm::vec4(verts[f[0] - 1], 1.0f), glm::vec4(verts[f[1] - 1], 1.0f), glm::vec4(verts[f[2] - 1], 1.0f)};
                     numFilledTris++;
                     if (numFilledTris > mMesh->numTris)
                     {
@@ -331,21 +329,6 @@ void listDir(const char *dirname, uint8_t levels)
     }
 }
 
-void multiplyMatrixVector(const vec3d &i, vec3d &o, const mat4 &m)
-{
-    o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-    o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-    o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-    float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
-
-    if (w != 0.0f)
-    {
-        o.x = o.x / w;
-        o.y = o.y / w;
-        o.z = o.z / w;
-    }
-}
-
 int compareTris(const void *t1Ptr, const void *t2Ptr)
 {
     const triangle *t1 = reinterpret_cast<const triangle *>(t1Ptr);
@@ -373,21 +356,24 @@ class ESPCon
     const uint16_t width = tft.width();
 #endif
 
-    mesh *mMesh = nullptr;
+    mesh *mMesh;
 
-    triangle *trisToRaster = nullptr;
-    uint32_t numTrisToRaster = 0;
+    triangle *trisToRaster;
+    uint32_t numTrisToRaster;
 
-    mat4 matProj{};
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view;
+    glm::mat4 projection;
 
-    vec3d vCamera{};
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    Camera camera = Camera(cameraPos);
 
     uint32_t deltaTime;
     uint32_t elapsedTime;
     uint32_t lastTime;
-    float theta = 0.0f;
+    float theta;
 
-    color col{};
+    color col;
 
 public:
     ESPCon() {}
@@ -411,6 +397,11 @@ public:
             return 1;
         }
 
+        for (constexpr uint8_t button : buttons)
+        {
+            pinMode(button, INPUT);
+        }
+
 #ifdef BUILD_ST7735
         tft.initR(INITR_144GREENTAB);
 
@@ -425,18 +416,11 @@ public:
 
         elapsedTime = millis();
 
-        constexpr float fNear = 0.1f;
-        constexpr float fFar = 1000.0f;
-        constexpr float fFov = 90.0f;
         const float fAspectRatio = static_cast<float>(height) / width;
-        const float fFovRad = 1.0f / tan(fFov * 0.5f / 180.0f * M_PI);
 
-        matProj.m[0][0] = fAspectRatio * fFovRad;
-        matProj.m[1][1] = fFovRad;
-        matProj.m[2][2] = fFar / (fFar - fNear);
-        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
-        matProj.m[2][3] = 1.0f;
-        matProj.m[3][3] = 0.0f;
+        projection = glm::perspective(glm::radians(camera.Zoom), fAspectRatio, 0.1f, 100.0f);
+
+        view = camera.GetViewMatrix();
 
         mMesh = loadObj("/littlefs/ship.obj");
         if (mMesh == nullptr)
@@ -470,27 +454,22 @@ public:
         theta = static_cast<float>(elapsedTime) / 1000;
         // Serial.println(1000.0f/deltaTime, DEC); // FPS
 
+        int button1State = digitalRead(buttons[0]);
+        int button2State = digitalRead(buttons[1]);
+
+        // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+        if (button1State == HIGH)
+        {
+            camera.ProcessKeyboard(LEFT, deltaTime);
+        }
+        else if (button2State == HIGH)
+        {
+            camera.ProcessKeyboard(RIGHT, deltaTime);
+        }
+
         col.r = sin(theta * 2.0f) * 31;
         col.g = sin(theta * 0.7f) * 63;
         col.b = sin(theta * 1.3f) * 31;
-
-        mat4 matRotZ, matRotX;
-
-        // Rotation Z
-        matRotZ.m[0][0] = cos(theta);
-        matRotZ.m[0][1] = sin(theta);
-        matRotZ.m[1][0] = -sin(theta);
-        matRotZ.m[1][1] = cos(theta);
-        matRotZ.m[2][2] = 1;
-        matRotZ.m[3][3] = 1;
-
-        // Rotation X
-        matRotX.m[0][0] = 1;
-        matRotX.m[1][1] = cos(theta * 0.5f);
-        matRotX.m[1][2] = sin(theta * 0.5f);
-        matRotX.m[2][1] = -sin(theta * 0.5f);
-        matRotX.m[2][2] = cos(theta * 0.5f);
-        matRotX.m[3][3] = 1;
 
         memset(trisToRaster, 0, sizeof(triangle) * mMesh->numTris);
         numTrisToRaster = 0;
@@ -498,74 +477,52 @@ public:
         // Draw triangles
         for (uint32_t i = 0; i < mMesh->numTris; i++)
         {
-            triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
-
-            // Rotate Z
-            multiplyMatrixVector(mMesh->tris[i].p[0], triRotatedZ.p[0], matRotZ);
-            multiplyMatrixVector(mMesh->tris[i].p[1], triRotatedZ.p[1], matRotZ);
-            multiplyMatrixVector(mMesh->tris[i].p[2], triRotatedZ.p[2], matRotZ);
-
-            // Rotate X
-            multiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
-            multiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
-            multiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+            triangle triTransformed, triViewed, triProjected;
 
             // Translate triangles
-            triTranslated = triRotatedZX;
-            triTranslated.p[0].z = triTranslated.p[0].z + 8.0f;
-            triTranslated.p[1].z = triTranslated.p[1].z + 8.0f;
-            triTranslated.p[2].z = triTranslated.p[2].z + 8.0f;
+            triTransformed.p[0] = model * mMesh->tris[i].p[0];
+            triTransformed.p[1] = model * mMesh->tris[i].p[1];
+            triTransformed.p[2] = model * mMesh->tris[i].p[2];
 
             // Check if side is visible
-            vec3d normal, line1, line2;
+            glm::vec3 normal, line1, line2;
 
-            line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
-            line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
-            line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
+            line1 = triTransformed.p[1] - triTransformed.p[0];
+            line2 = triTransformed.p[2] - triTransformed.p[0];
 
-            line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
-            line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
-            line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
+            normal = glm::cross(line1, line2);
+            normal = glm::normalize(normal);
 
-            normal.x = line1.y * line2.z - line1.z * line2.y;
-            normal.y = line1.z * line2.x - line1.x * line2.z;
-            normal.z = line1.x * line2.y - line1.y * line2.x;
+            glm::vec3 vCameraRay = glm::vec3(triTransformed.p[0]) - camera.Position;
 
-            float l = sqrt(pow(normal.x, 2) + pow(normal.y, 2) + pow(normal.z, 2));
-            normal.x /= l;
-            normal.y /= l;
-            normal.z /= l;
-
-            if ((normal.x * (triTranslated.p[0].x - vCamera.x) +
-                 normal.y * (triTranslated.p[0].y - vCamera.y) +
-                 normal.z * (triTranslated.p[0].z - vCamera.z)) < 0.0f)
+            if (glm::dot(normal, vCameraRay) < 0.0f)
             {
                 // Add basic lighting
-                vec3d light_direction = {0.0f, 0.0f, -1.0f};
+                glm::vec3 light_direction = {0.0f, 1.0f, -1.0f};
+                light_direction = glm::normalize(light_direction);
 
-                float l = sqrt(pow(light_direction.x, 2) + pow(light_direction.y, 2) +
-                               pow(light_direction.z, 2));
-                light_direction.x /= l;
-                light_direction.y /= l;
-                light_direction.z /= l;
-
-                float dp = normal.x * light_direction.x + normal.y * light_direction.y +
-                           normal.z * light_direction.z;
+                float dp = max(0.1f, glm::dot(light_direction, normal));
 
                 uint16_t c = col.getColor(dp);
                 triProjected.col = c;
 
-                multiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
-                multiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
-                multiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+                triViewed.p[0] = view * triTransformed.p[0];
+                triViewed.p[1] = view * triTransformed.p[1];
+                triViewed.p[2] = view * triTransformed.p[2];
+
+                triProjected.p[0] = projection * triViewed.p[0];
+                triProjected.p[1] = projection * triViewed.p[1];
+                triProjected.p[2] = projection * triViewed.p[2];
+
+                triProjected.p[0] /= triProjected.p[0].w;
+                triProjected.p[1] /= triProjected.p[1].w;
+                triProjected.p[2] /= triProjected.p[2].w;
 
                 // Scale into view
-                triProjected.p[0].x += 1.0f;
-                triProjected.p[0].y += 1.0f;
-                triProjected.p[1].x += 1.0f;
-                triProjected.p[1].y += 1.0f;
-                triProjected.p[2].x += 1.0f;
-                triProjected.p[2].y += 1.0f;
+                glm::vec3 vOffsetView = {1.0f, 1.0f, 0.0f};
+                triProjected.p[0] += vOffsetView;
+                triProjected.p[1] += vOffsetView;
+                triProjected.p[2] += vOffsetView;
 
                 triProjected.p[0].x *= 0.5f * static_cast<float>(width);
                 triProjected.p[1].x *= 0.5f * static_cast<float>(width);
